4edaa22cde0bf567349c9778ab72f33953c83d34
Saladin Afoh <safoh@student.codam.nl>
2023-09-08 09:31:32 +0200

    colors

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -27,7 +27,7 @@ std::string Logger::logLevelToString(LogLevel lvl)
 	case LogLevel::DEBUG:
 		return Color::cyan + "DEBUG";
 	case LogLevel::INFO:
-		return Color::white + "INFO";
+		return "INFO";
 	case LogLevel::WARNING:
 		return Color::yellow + "WARNING";
 	case LogLevel::ERROR:
@@ -43,12 +43,10 @@ std::string Logger::getTimestamp()
 {
 	auto now = std::chrono::system_clock::now();
 	auto time = std::chrono::system_clock::to_time_t(now);
+	std::ostringstream oss;
 
 	std::tm tm;
 	localtime_r(&time, &tm);
-
-	std::ostringstream oss;
 	oss << std::put_time(&tm, "%Y-%m-%d %X");
-
 	return oss.str();
 }
