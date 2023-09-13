c85f3c4c0eb5d3438bed51e29a729e51afe8aa67
Saladin Afoh <safoh@student.codam.nl>
2023-09-13 16:25:07 +0200

    thread pool creation

--- include/ThreadPool.hpp
+++ include/ThreadPool.hpp
@@ -1,12 +1,29 @@
 #ifndef THREADPOOL_HPP
 #define THREADPOOL_HPP
 
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
 class ThreadPool
 {
   private:
-	unsigned int _thread_count;
+	bool _stop;
+	std::size_t _thread_count;
+	std::vector<std::thread> _threads;
+	std::mutex _queue_mutex;
+	std::condition_variable _condition;
+	std::queue<std::function<void()>> _tasks;
+
+	void ThreadLoop();
 
   public:
+	void Start();
+	void QueueTask(const std::function<void()> &task);
+	void Stop();
+	bool busy();
 	ThreadPool() = delete;
 	ThreadPool(const unsigned int &thread_count);
 	ThreadPool(const ThreadPool &src) = delete;
--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,11 +1,76 @@
+#include <Logger.hpp>
 #include <ThreadPool.hpp>
+#include <iostream>
 #include <thread>
 
 ThreadPool::ThreadPool(const unsigned int &thread_count)
-	: _thread_count(thread_count)
+	: _stop(false), _thread_count(thread_count)
 {
 }
 
 ThreadPool::~ThreadPool()
 {
+}
+
+void ThreadPool::Start()
+{
+	Logger &logger = Logger::getInstance();
+	if (_thread_count == 0)
+	{
+		logger.log(INFO, "Using maximum number of threads");
+		_thread_count = std::thread::hardware_concurrency();
+	}
+	for (std::size_t i = 0; i < _thread_count; ++i)
+	{
+		_threads.emplace_back(&ThreadPool::ThreadLoop, this);
+	}
+}
+
+void ThreadPool::ThreadLoop()
+{
+	while (true)
+	{
+		std::function<void()> task;
+		{
+			{
+				std::unique_lock<std::mutex> lock(_queue_mutex);
+				_condition.wait(lock,
+								[this] { return !_tasks.empty() || _stop; });
+			}
+			if (_stop == true)
+				return;
+			task = _tasks.front();
+			_tasks.pop();
+		}
+	}
+}
+
+void ThreadPool::QueueTask(const std::function<void()> &task)
+{
+	std::unique_lock<std::mutex> lock(_queue_mutex);
+	_tasks.push(task);
+	_condition.notify_one();
+}
+
+bool ThreadPool::busy()
+{
+	bool poolbusy;
+	{
+		std::unique_lock<std::mutex> lock(_queue_mutex);
+		poolbusy = !_tasks.empty();
+	}
+	return (poolbusy);
+}
+void ThreadPool::Stop()
+{
+	{
+		std::unique_lock<std::mutex> lock(_queue_mutex);
+		_stop = true;
+	}
+	_condition.notify_all();
+	for (std::thread &thread : _threads)
+	{
+		thread.join();
+	}
+	_threads.clear();
 }
