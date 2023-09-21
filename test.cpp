70da252d5b624681f347001a4b7d835cf28d5a17
Lucien Van Bussel <lvan-bus@f1r6s8.codam.nl>
2023-09-21 11:34:42 +0200

    now able to check if the image is in the file system, if so post it. Also able to post text messages.

--- test.cpp
+++ test.cpp
@@ -3,16 +3,16 @@
 #include <vector>
 #include <string>
 
-void	post_data(const std::string filename, const std::string post_data) 
+void	post_image(const std::string filename, const std::string post_data) 
 {
 	std::ifstream		inputFile(filename);
 	std::ofstream		outputFile("temp.txt");
-	std::ofstream		outData(post_data)
+	std::ifstream		image("data/" + post_data);
 	const std::string	check_point = "        <!-- IMAGES -->";
 	std::string			converted_post_data = "        <img src=\"/home/lvan-bus/Documents/webserv/data/";
 	bool				done = true;
 
-	if (!inputFile || !outputFile) {
+	if (!inputFile || !outputFile || !image) {
 		std::cerr << "Error opening files." << std::endl;
 		return ;
     }
@@ -48,10 +48,64 @@ void	post_data(const std::string filename, const std::string post_data)
     std::cout << "Text inserted successfully." << std::endl;
 }
 
-int main(void) {
-    // const std::string filename = "example.txt";
-	const std::string filename = "data/www/index.html";
-    const std::string   post_message = "nose_monkey.webp";
-    post_data(filename, post_message);
+
+void	post_text(const std::string filename, const std::string content)
+{
+	std::ifstream		inputFile(filename);
+	std::ofstream		outputFile("temp.txt");
+	const std::string	check_point = "        <!-- text/plain -->";
+	std::string			converted_post_data = "        <P ALIGN=\"LEFT\">";
+	bool				done = true;
+
+	if (!inputFile || !outputFile) {
+		std::cerr << "Error opening files." << std::endl;
+		return ;
+    }
+	converted_post_data += content + "</P>";
+	
+	std::string line;
+	std::vector<std::string> lines;
+	int currentLine = 0;
+
+	while (std::getline(inputFile, line)) {
+		currentLine++;
+		if (line == check_point && done) {
+			lines.push_back(line);
+			lines.push_back(converted_post_data);
+			done = false;
+			continue ;
+		}
+		lines.push_back(line);
+	}
+
+    for (const std::string& storedLine : lines) {
+        outputFile << storedLine << '\n';
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    if (std::rename("temp.txt", filename.c_str()) != 0) {
+        std::cerr << "Error renaming the file." << std::endl;
+        return ;
+    }
+
+    std::cout << "Text inserted successfully." << std::endl;
+}
+
+int main(int argc, char **argv) {
+	if (argc != 3)
+	{
+		std::cout << "Invalid number of parameters.\n";
+		return 0;
+	}
+	const std::string	filename = "data/www/index.html";
+    const std::string   post_message = argv[2];
+	const std::string	content_type = argv[1];
+	if (content_type == "image") {
+		 post_image(filename, post_message);
+	}
+	else if (content_type == "text")
+		post_text(filename, post_message);
 	return 0;
 }
