7d82f249f35e6b58d3a6841d4e2a5277b9633c45
Saladin Afoh <safoh@f1r5s8.codam.nl>
2023-09-26 13:56:59 +0200

    Added test for both servers

--- tests/get_request.sh
+++ tests/get_request.sh
@@ -2,10 +2,12 @@
 
 # Server configuration
 SERVER_HOST="localhost"
-SERVER_PORT="18000"
+SERVER_PORT="6969"
 
 # Request file
 REQUEST_FILE="tests/request/get.txt"
 
 # Use netcat to send the contents of the request file to the server
+nc "${SERVER_HOST}" "${SERVER_PORT}" < "${REQUEST_FILE}"
+SERVER_PORT="9696"
 nc "${SERVER_HOST}" "${SERVER_PORT}" < "${REQUEST_FILE}"
