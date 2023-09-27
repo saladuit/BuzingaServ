deb19726a5eff2cf9762298d786860730951b802
Martijn Weverling <mweverli@student.codam.nl>
2023-09-27 13:13:08 +0200

    GET_local.sh: if no port is given in $1 it will default to 9696

--- tests/GET_local.sh
+++ tests/GET_local.sh
@@ -1,6 +1,17 @@
-echo "GET REQUEST on localhost at port $1"
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+	SERVER_PORT="9696"
+else
+	SERVER_PORT=$1
+fi
+
+HTTP_VERSION="http1.1"
+
+SERVER_HOST="localhost"
+SERVER_LOG_DIR=../build/log/
+
+echo "GET REQUEST on localhost at port $SERVER_PORT"
 echo ""
 
-HTTP_LOG_DIR=../build/log/
-
-curl localhost:$1 --http1.1 -s -o $HTTP_LOG_DIR/GET_${1}_body.log -D $HTTP_LOG_DIR/GET_${1}_header.log
+curl $SERVER_HOST:$SERVER_PORT --$HTTP_VERSION -s -o $SERVER_LOG_DIR/GET_${1}_body.log -D $SERVER_LOG_DIR/GET_${1}_header.log
